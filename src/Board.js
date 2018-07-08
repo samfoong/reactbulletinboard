a47a6dc0e59eb297a8e434b8c0417b6710df335e
samfoong <samfoong88@gmail.com>
2018-07-08 23:57:38 +1000

    Function to add notes

--- src/Board.js
+++ src/Board.js
@@ -1,64 +1,75 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes: [
-                {
-                    id: 0,
-                    note: "Call Lisa"
-                },
-                {
-                    id: 1,
-                    note: "Email John"
-                },
-                {
-                    id: 2,
-                    note: "Order printer ink"
-                }
-            ]
-        }
-        this.eachNote = this.eachNote.bind(this)
-        this.update = this.update.bind(this)
-        this.remove = this.remove.bind(this)
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			notes: []
+		}
+		this.add = this.add.bind(this)
+		this.eachNote = this.eachNote.bind(this)
+		this.update = this.update.bind(this)
+		this.remove = this.remove.bind(this)
+		this.nextId = this.nextId.bind(this)
+	}
 
-    update(newText, i) {
-        console.log('updating item at index', i, newText)
-        this.setState(prevState => ({
-            notes: prevState.notes.map(
-                note => (note.id !==i) ? note : {...note, note: newText}
-            )
-        }))
-    }
+	add(text) {
+		this.setState(prevState => ({
+			notes: [
+				...prevState.notes,
+				{
+					id: this.nextId(),
+					note: text
+				}
+			]
+		}))
+	}
 
-    remove(id) {
-        console.log('removing item at', id)
-        this.setState(prevState => ({
-            notes: prevState.notes.filter(note => note.id !== id)
-        }))
-    }
+	nextId() {
+		this.uniqueId = this.uniqueId || 0
+		return this.uniqueId++
+	}
 
-    eachNote(note, i) {
-        return (
-            <Note key={i}
-                index={i}
-                onChange={this.update}
-                onRemove={this.remove}>
-                {note.note}
-            </Note>
-        )
-    }
+	update(newText, i) {
+		console.log('updating item at index', i, newText)
+		this.setState(prevState => ({
+			notes: prevState.notes.map(
+				note => (note.id !== i) ? note : {...note, note: newText}
+			)
+		}))
+	}
 
-    render() {
-        return (
-            <div className="board">
-                {this.state.notes.map(this.eachNote)}
-            </div>
-        )
-    }
+	remove(id) {
+		console.log('removing item at', id)
+		this.setState(prevState => ({
+			notes: prevState.notes.filter(note => note.id !== id)
+		}))
+	}
+
+	eachNote(note, i) {
+		return (
+			<Note key={i}
+				  index={i}
+				  onChange={this.update}
+				  onRemove={this.remove}>
+				  {note.note}
+		    </Note>
+		)
+	}
+
+	render() {
+		return (
+			<div className="board">
+				{this.state.notes.map(this.eachNote)}
+				<button onClick={this.add.bind(null, "New Note")}
+						id="add">
+					<FaPlus />
+				</button>
+			</div>
+		)
+	}
 }
 
 export default Board
