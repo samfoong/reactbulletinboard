5f564817f972228b024a5aecabd0fa47c70e9204
samfoong <samfoong88@gmail.com>
2018-07-08 23:38:18 +1000

    Ability to update notes

--- src/Board.js
+++ src/Board.js
@@ -7,26 +7,37 @@ class Board extends Component {
         this.state = {
             notes: [
                 {
-                    id: 33,
+                    id: 0,
                     note: "Call Lisa"
                 },
                 {
-                    id: 34,
+                    id: 1,
                     note: "Email John"
                 },
                 {
-                    id:35,
+                    id: 2,
                     note: "Order printer ink"
                 }
             ]
         }
         this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+    }
+
+    update(newText, i) {
+        console.log('updating item at index', i, newText)
+        this.setState(prevState => ({
+            notes: prevState.notes.map(
+                note => (note.id !==i) ? note : {...note, note: newText}
+            )
+        }))
     }
 
     eachNote(note, i) {
         return (
             <Note key={i}
-                index={i}>
+                index={i}
+                onChange={this.update}>
                 {note.note}
             </Note>
         )
